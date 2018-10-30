b82fa8c9545f96c6b02f917e2f77cd7f9211cce5
Miguel Alegria <mikel063093@gmail.com>
2018-10-30 12:43:57 -0500

    Initial Commit

---
+++ todo1test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todo1test
+//
+//  Created by miguel alegria on 10/30/18.
+//  Copyright © 2018 miguel alegria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
